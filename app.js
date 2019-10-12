050d8319ec8228617082a8e12cdbfbcb74cae94d
Ana Morais <ana.bulario.morais@gmail.com>
2019-10-13 00:19:56 +0100

    exemple-class, new,constructor

---
+++ app.js
@@ -0,0 +1,34 @@
+class Rectangle {
+    // TODO
+    constructor (width, height){
+    this.width = width;
+    this.height = height;
+    }
+    calculatePerimeter() {
+        let perimeter = this.width*2+this.height*2;
+    return perimeter
+    }
+    calculateArea() {
+        let area = this.width*this.height
+        return area;
+    }
+  }
+  
+  class Square extends Rectangle {
+    // TODO
+    constructor(side) {
+        super(side, side);
+    }
+  }
+  
+  let r1 = new Rectangle(6,7)
+  console.log('Perimeter of r1 =', r1.calculatePerimeter()) // 26
+  console.log('Area of r1 =', r1.calculateArea()) // 42
+  
+  var s1 = new Square(5)
+  console.log('Perimeter of s1 =', s1.calculatePerimeter()) // 20
+  console.log('Area of s1 =', s1.calculateArea()) // 25
+  
+  var s2 = new Square(10)
+  console.log('Perimeter of s2 =', s2.calculatePerimeter()) // 40
+  console.log('Area of s2 =', s2.calculateArea()) // 100
